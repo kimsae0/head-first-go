5e82deaabd54829deea6f0b07ed552051387e45d
Matthew Brahms <matthewbrahms@gmail.com>
2019-08-05 14:27:54 -0500

    Saving work on paint-calculator

---
+++ ch-3/01-paint-calculator/paint-calculator.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var metersPerLiter float64
+
+func paintNeeded(width float64, height float64) float64 {
+	area := width * height
+	return area / metersPerLiter
+}
+
+func main() {
+	metersPerLiter = 10.0
+
+	var amount, total float64
+
+	amount = paintNeeded(4.2, 3.0)
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+
+	amount = paintNeeded(5.2, 3.5)
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+
+	fmt.Printf("Total: %0.2f liters\n", total)
+}
