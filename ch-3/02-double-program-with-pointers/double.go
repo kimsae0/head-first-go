043800835d08826a749dee80046dec878e494716
Matthew Brahms <matthewbrahms@gmail.com>
2019-08-16 15:26:38 -0500

    finished chapter 3

---
+++ ch-3/02-double-program-with-pointers/double.go
@@ -0,0 +1,14 @@
+// Package double takes a given number and doubles it.
+package main
+
+import "fmt"
+
+func main() {
+	amount := 6
+	double(&amount)
+	fmt.Println(amount)
+}
+
+func double(number *int) {
+	*number *= 2
+}
