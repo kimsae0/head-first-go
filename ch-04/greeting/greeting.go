11939d1e279a2896319beee95c8fb10778ba50b5
Matthew Brahms <matthewbrahms@gmail.com>
2020-03-03 11:49:30 -0600

    package tinkering

---
+++ ch-04/greeting/greeting.go
@@ -0,0 +1,14 @@
+// Package greeting exports functions for saying hello and hi
+package greeting
+
+import "fmt"
+
+// Hello prints the word "Hello!"
+func Hello() {
+	fmt.Println("Hello!")
+}
+
+// Hi prints the work "Hi!"
+func Hi() {
+	fmt.Println("Hi!")
+}
---
+++ ch-04/hi/main.go
@@ -0,0 +1,11 @@
+//
+package main
+
+import (
+	"github.com/matthewbrahms/head-first-go/ch-04/greeting"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
